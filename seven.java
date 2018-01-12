c036fca9fc7f58a7b91b180d48376288d871001f
parthivvutukuru1196 <35352967+parthivvutukuru1196@users.noreply.github.com>
2018-01-11 16:31:40 -0800

    Add files via upload
    First Java File

---
+++ seven.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+class sample
+{
+
+
+void disp(int a, int b)
+{
+System.out.println(a+b);
+}
+}
+class demo extends sample
+{
+void disp(int a, int b)
+{
+System.out.println(a*b);
+}
+
+public static void main(String[] args)
+{
+demo d = new demo();
+sample s = new demo();
+d.disp(10,20);
+s.disp(5,5);
+}
+}
